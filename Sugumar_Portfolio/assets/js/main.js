0d9d42c02e0e49db0b1e97df1df81d4a7983a3ec
Sugumar C <165118729+Sugumar63@users.noreply.github.com>
2024-04-16 19:50:14 +0530

    Add files via upload

---
+++ Sugumar_Portfolio/assets/js/main.js
@@ -0,0 +1,46 @@
+// HOME TYPING....
+
+var typed = new Typed(".text", {
+      strings: ["Front-End Web"],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 1000,
+      loop: true
+})
+
+var typed = new Typed(".text1", {
+      strings: ["Developer!"],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 1000,
+      loop: true
+})
+
+// SHOW MENU
+
+const showMenu = (toggleId, navId) => {
+      const toggle = document.getElementById(toggleId),
+            nav = document.getElementById(navId)
+
+      if (toggle && nav) {
+            toggle.addEventListener('click', () => {
+                  nav.classList.toggle('show')
+            });
+      }
+}
+
+showMenu('nav_toggle', 'nav_menu')
+
+// ACTIVE & REMOVE ACTIVE
+const navLink = document.querySelectorAll('.nav_link')
+navLink.forEach(n => n.classList.remove('active'))
+
+function linkAction() {
+      navLink.forEach(n => n.classList.remove('active'))
+      this.classList.add('active')
+
+      const navMenu = document.getElementById('nav_menu')
+      navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
